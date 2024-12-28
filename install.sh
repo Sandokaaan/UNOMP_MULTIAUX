1ee213ed5a5caf0ca6cf70e11094f92b4896bf86
Sandokaaan <geo@vosar.cz>
2024-12-28 23:48:23 +0100

    initial upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# prerequirements 
+# sudo apt-get update
+# sudo apt-get install git redis nano wget curl ntp screen
+# sudo apt-get install build-essential libtool autotools-dev autoconf pkg-config libssl-dev
+# sudo apt-get install libboost-all-dev npm nodejs libminiupnpc-dev software-properties-common
+
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+source ~/.bashrc
+nvm install 0.10.29
+nvm use 0.10.29
+nvm alias default 0.10.29
+
+# allready cloned
+# git clone https://github.com/Sandokaaan/UNOMP_MULTIAUX.git
+
+echo 'Set required password in files config.json and pool_config/litecoin.json'
+
