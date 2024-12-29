314d8732dc52dcfa3574505bf46c29b9a81d8a13
Sandokaaan <30953425+Sandokaaan@users.noreply.github.com>
2024-12-29 01:19:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,14 @@
 # sudo apt-get install build-essential libtool autotools-dev autoconf pkg-config libssl-dev
 # sudo apt-get install libboost-all-dev npm nodejs libminiupnpc-dev software-properties-common
 
+cd ~
+mkdir -p .nvm
+mkdir -p .npm
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-source ~/.bashrc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 nvm install 0.10.29
 nvm use 0.10.29
 nvm alias default 0.10.29
@@ -16,4 +22,3 @@ nvm alias default 0.10.29
 # git clone https://github.com/Sandokaaan/UNOMP_MULTIAUX.git
 
 echo 'Set required password in files config.json and pool_config/litecoin.json'
-
